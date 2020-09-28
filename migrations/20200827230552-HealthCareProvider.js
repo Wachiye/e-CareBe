25b789a9f24f51f4182158284ed93e4b708135f6
OGUNSOLA USMAN ADEBAYO <57511444+ogunsoladebayo@users.noreply.github.com>
2020-09-28 18:51:33 +0100

    Update 20200827230552-HealthCareProvider.js

--- migrations/20200827230552-HealthCareProvider.js
+++ migrations/20200827230552-HealthCareProvider.js
@@ -10,161 +10,81 @@ module.exports = {
 		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
 		 */
 
-<<<<<<< HEAD
-    return queryInterface.createTable("health_care_providers", {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      name: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      address_line1: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      accreditation_no: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      hotline: {
-        type: Sequelize.STRING,
-        allowNull: true,
-      },
-      private_govt: {
-        type: Sequelize.ENUM,
-        values: ["private", "government"],
-        allowNull: true,
-      },
-      occupation: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      long: {
-        type: Sequelize.DOUBLE,
-        allowNull: true,
-      },
-      lat: {
-        type: Sequelize.DOUBLE,
-        allowNull: true,
-      },
-      healthcare_type: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      address_line2: {
-        type: Sequelize.TEXT,
-        allowNull: true,
-      },
-      city: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      state: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      country: {
-        type: Sequelize.STRING(255),
-        allowNull: true,
-      },
-      zipcode: {
-        type: Sequelize.INTEGER,
-        allowNull: true,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal(
-          "CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"
-        ),
-      },
-    });
-  },
-=======
-		return queryInterface.createTable('health_care_providers', {
-			id: {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-			},
-			name: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			address_line1: {
-				type: Sequelize.TEXT,
-				allowNull: true,
-			},
-			accreditation_no: {
-				type: Sequelize.STRING,
-				allowNull: true,
-			},
-			hotline: {
-				type: Sequelize.STRING,
-				allowNull: true,
-			},
-			private_govt: {
-				type: Sequelize.ENUM,
-				values: ['private', 'government'],
-				allowNull: true,
-			},
-			occupation: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			long: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			lat: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			healthcare_type: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			address_line2: {
-				type: Sequelize.TEXT,
-				allowNull: true,
-			},
-			city: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			state: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			country: {
-				type: Sequelize.STRING(255),
-				allowNull: true,
-			},
-			zipcode: {
-				type: Sequelize.INTEGER,
-				allowNull: true,
-			},
-			created_at: {
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
-			},
-			updated_at: {
-				type: Sequelize.DATE,
-				defaultValue: Sequelize.literal(
-					'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
-				),
-			},
-		});
+	return queryInterface.createTable('health_care_providers', {
+		patient_id: {
+			type: Sequelize.STRING(255),
+			allowNull: false,
+			primaryKey: true,
+		},
+		name: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		address_line1: {
+			type: Sequelize.TEXT,
+			allowNull: true,
+		},
+		accreditation_no: {
+			type: Sequelize.STRING,
+			allowNull: true,
+		},
+		hotline: {
+			type: Sequelize.STRING,
+			allowNull: true,
+		},
+		private_govt: {
+			type: Sequelize.ENUM,
+			values: ['private', 'government'],
+			allowNull: true,
+		},
+		password: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		long: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		lat: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		healthcare_type: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		address_line2: {
+			type: Sequelize.TEXT,
+			allowNull: true,
+		},
+		city: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		state: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		country: {
+			type: Sequelize.STRING(255),
+			allowNull: true,
+		},
+		zipcode: {
+			type: Sequelize.INTEGER,
+			allowNull: true,
+		},
+		created_at: {
+			type: Sequelize.DATE,
+			defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+		},
+		updated_at: {
+			type: Sequelize.DATE,
+			defaultValue: Sequelize.literal(
+				'CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'
+			),
+		},
+	});
 	},
->>>>>>> b36818962fac7d2309e619298d93f103b7fdc8bb
 
 	down: async (queryInterface, Sequelize) => {
 		/**
